fcf875230cac9b48856a30c539904ec71d472ad2
Rizwan-Hasan <rizwan.hasan486@gmail.com>
2019-06-09 17:18:39 +0600

    updated

--- app/src/main/java/com/cgpacalc/MainActivity.kt
+++ app/src/main/java/com/cgpacalc/MainActivity.kt
@@ -2,6 +2,7 @@ package com.cgpacalc
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -9,9 +10,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(
+            applicationContext,
+            "Developed by Rizwan Hasan",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
 
-        Toast.makeText(applicationContext, "Developed by Rizwan Hasan", Toast.LENGTH_SHORT).show()
-
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 
 }
