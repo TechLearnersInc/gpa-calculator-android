6cb99748a00446d5ae6ada73a1229574b09dafd6
Rizwan-Hasan <rizwan.hasan486@gmail.com>
2019-06-18 01:36:44 +0600

    updated

---
+++ app/src/main/java/com/cgpacalc/AboutActivity.kt
@@ -0,0 +1,12 @@
+package com.cgpacalc
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AboutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+}
