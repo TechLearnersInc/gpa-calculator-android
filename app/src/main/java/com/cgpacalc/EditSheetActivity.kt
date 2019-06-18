96499b131111d9c9eca9d3606c4818acc7167de4
Rizwan-Hasan <rizwan.hasan486@gmail.com>
2019-06-19 02:53:37 +0600

    updated

--- app/src/main/java/com/cgpacalc/EditSheetActivity.kt
+++ app/src/main/java/com/cgpacalc/EditSheetActivity.kt
@@ -1,25 +1,75 @@
 package com.cgpacalc
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import android.widget.ArrayAdapter
+import android.view.LayoutInflater
+import android.view.Menu
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_edit_sheet.*
+import kotlinx.android.synthetic.main.gpa_input_dialogue.view.*
+import java.lang.NumberFormatException
 
 class EditSheetActivity : AppCompatActivity() {
-
-    private var dbManager: DbManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_sheet)
 
-        // Database
-        this.dbManager = DbManager(applicationContext, MainActivity().dbVersion)
-        val x: MutableList<HashMap<String, Float>> = this.dbManager!!.readDataToList()
-        Log.i("FARIA", x.toString())
-
-        val adaptar = GradeListViewAdaptar(this, x)
-        MyListView.adapter = adaptar
+        // Grade Sheet Handler
+        this.gradeListViewHandler()
     }
+
+    // Adding three dot navigation menu to this activity
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.edit_sheet_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    // Grade Sheet Handler
+    @SuppressLint("InflateParams")
+    private fun gradeListViewHandler() {
+        val adaptar = GradeListViewAdaptar(this@EditSheetActivity)
+        MyListView.adapter = adaptar
+
+        MyListView.setOnItemClickListener { parent, view, position, id ->
+            run {
+                val inputView = LayoutInflater.from(this@EditSheetActivity).inflate(R.layout.gpa_input_dialogue, null)
+
+                val gradeList = adaptar.listOfGrade!![position]
+                val grade: String = gradeList.keys.toString().substring(1, gradeList.keys.toString().length - 1)
+                val gpa: String = gradeList.values.toString().substring(1, gradeList.values.toString().length - 1)
+
+                inputView.gradeInputDialogue.text = grade
+                inputView.gpaInputDialogue.setText(gpa)
+
+                val inputDialogue = AlertDialog.Builder(this@EditSheetActivity)
+                inputDialogue.setTitle("Edit GPA")
+                inputDialogue.setView(inputView)
+                inputDialogue.setCancelable(true)
+                inputDialogue.setNegativeButton("Cancel") { dialogInterface, _ -> dialogInterface.cancel() }
+                inputDialogue.setPositiveButton("Ok") { _, _ ->
+                    run {
+                        try {
+                            val tmp: String = inputView.gpaInputDialogue.text.toString()
+                            if (gpa != tmp) {
+                                adaptar.listOfGrade!![position][grade] =
+                                    inputView.gpaInputDialogue.text.toString().toFloat()
+                                adaptar.notifyDataSetChanged()
+                                Toast.makeText(this@EditSheetActivity, "Value changed", Toast.LENGTH_LONG).show()
+                            } else {
+                                Toast.makeText(this@EditSheetActivity, "No change", Toast.LENGTH_LONG).show()
+                            }
+                        } catch (e: NumberFormatException) {
+                            e.printStackTrace()
+                            Toast.makeText(this@EditSheetActivity, "Value error", Toast.LENGTH_LONG).show()
+                        }
+                    }
+                }
+                inputDialogue.show()
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/cgpacalc/GradeListViewAdaptar.kt
+++ app/src/main/java/com/cgpacalc/GradeListViewAdaptar.kt
@@ -8,27 +8,32 @@ import android.view.ViewGroup
 import android.widget.BaseAdapter
 import kotlinx.android.synthetic.main.grade_listview_ticket.view.*
 
-class GradeListViewAdaptar(thisContext: Context, x: MutableList<HashMap<String, Float>>) : BaseAdapter() {
-    private val listOfGrade = x
-    private val context: Context = thisContext
+open class GradeListViewAdaptar(thisContext: Context) : BaseAdapter() {
+
+    private var context: Context = thisContext
+    var listOfGrade: MutableList<HashMap<String, Float>>? = null
+
+    init {
+        this.refreshDatabase()
+    }
 
     @SuppressLint("ViewHolder", "InflateParams")
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val gradeList = this.listOfGrade[position]
-        val inflator = this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        val myview = inflator.inflate(R.layout.grade_listview_ticket, null)
+        val inflator: LayoutInflater = this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val myview: View = inflator.inflate(R.layout.grade_listview_ticket, null)
 
+        val gradeList: HashMap<String, Float> = this.listOfGrade!![position]
         val grade: String = gradeList.keys.toString().substring(1, gradeList.keys.toString().length - 1)
         val gpa: String = gradeList.values.toString().substring(1, gradeList.values.toString().length - 1)
 
         myview.gradeTextView.text = grade
-        myview.gpaEditText.setText(gpa)
+        myview.gpaEditText.text = gpa
 
         return myview
     }
 
     override fun getItem(position: Int): Any {
-        return this.listOfGrade[position]
+        return this.listOfGrade!![position]
     }
 
     override fun getItemId(position: Int): Long {
@@ -36,6 +41,12 @@ class GradeListViewAdaptar(thisContext: Context, x: MutableList<HashMap<String, 
     }
 
     override fun getCount(): Int {
-        return listOfGrade.size
+        return this.listOfGrade!!.size
+    }
+
+    open fun refreshDatabase() {
+        // Database
+        val dbManager = DbManager(this.context, MainActivity().dbVersion)
+        this.listOfGrade = dbManager.readDataToList()
     }
 }
