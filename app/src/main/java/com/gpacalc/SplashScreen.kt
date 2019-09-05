82d7af70612141b52e720f19ea7cef2473f63c01
Rizwan-Hasan <rizwan.hasan486@gmail.com>
2019-09-05 20:03:16 +0600

    update

--- app/src/main/java/com/gpacalc/SplashScreen.kt
+++ app/src/main/java/com/gpacalc/SplashScreen.kt
@@ -13,7 +13,7 @@ class SplashScreen : AppCompatActivity() {
         val mainActivity = object : Thread() {
             override fun run() {
                 try {
-                    sleep(1000)
+                    sleep(2000)
                     startActivity(Intent(baseContext, MainActivity::class.java))
                     finish()
                 } catch (e: Exception) {
